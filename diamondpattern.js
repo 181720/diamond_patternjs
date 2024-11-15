91afc720341fd6e1bec8fdc7f516b9f06fd5f70e
Vinod Kumar <vinodkumar32826@gmail.com>
2024-11-16 00:18:46 +0530

    first commit

---
+++ diamondpattern.js
@@ -0,0 +1,27 @@
+for(var i = 0; i < 5; i++){
+    var str = ""
+    for(var j = 5; j > i; j--){
+        str += " ";
+    }
+    for(var k = 0; k <= i; k++){
+        str += "*";
+    }
+    for(var l = 1; l <= i; l++){
+        str += "*";
+    }
+    console.log(str);
+}
+//down patter
+for(var i = 5; i >= 0; i--){
+    var str = ""
+    for(var j = 5; j > i; j--){
+        str += " ";
+    }
+    for(var k = 0; k <= i; k++){
+        str += "*";
+    }
+    for(var l = 1; l <= i; l++){
+        str += "*";
+    }
+    console.log(str);
+}
